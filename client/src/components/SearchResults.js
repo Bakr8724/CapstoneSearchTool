34a871dc19e6093d6c25d9cef0742bea0329c0f1
Bakr <alkayalibakr@gmail.com>
2024-04-12 10:50:57 -0500

    slight update

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -3,13 +3,21 @@
 // It's responsible for the presentation layer, taking processed data and rendering them for the user.
 import React from 'react';
 
+// Local function to replace <b> tags with <strong> for better formatting
+const parseHtmlToJsx = (htmlString) => {
+    return htmlString.split(/<\/?b>/).map((part, index) => index % 2 === 1 ? <strong key={index}>{part}</strong> : part);
+};
+
 const SearchResults = ({ results }) => {
   return (
     <div className="search-results">
       <h2>Results:</h2>
       {results.length > 0 ? (
         results.map((result, index) => (
-          <div key={index} className="result-item">{result.title}</div>
+          <div key={index} className="result-item">
+            {/* Parse HTML title to JSX */}
+            <p>{parseHtmlToJsx(result.htmlTitle)} - <a href={result.link} target='_blank' rel="noopener noreferrer">Link</a></p>
+          </div>
         ))
       ) : (
         <p>No results found</p>
